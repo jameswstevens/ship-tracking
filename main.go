e0ebfa37c42ecef35bae5cd5e2408fbc8af2e26b
James Stevens <jsteve1@stanford.edu>
2023-06-25 23:00:13 -0400

    icon design and direction

--- main.go
+++ main.go
@@ -1,79 +1,83 @@
 package main
+
 import (
-"embed"
-"html/template"
-"io"
-"log"
-"net/http"
-"net/url"
-"os"
-"strconv"
+	"embed"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
 )
+
 //go:embed static
 var static embed.FS
+
 //go:embed templates
 var templates embed.FS
 var indexTemplate *template.Template
 var gmAPIKey string
 
 const datalasticBaseURL = "https://api.datalastic.com/api/v0/vessel_inradius?"
+
 var datalasticAPIKey string
-const datalasticAdditonalParams = "&type=fishing"
 
 func main() {
-gmAPIKey = os.Getenv("GM_API_KEY")
-datalasticAPIKey = os.Getenv("DATALASTIC_API_KEY")
-staticFS := http.FS(static)
-staticFileServer := http.FileServer(staticFS)
-var err error
-indexTemplate, err = template.ParseFS(templates, "templates/index.html")
-if err != nil {
- log.Fatalln(err)
-}
-mux := http.NewServeMux()
-mux.Handle("/static/", staticFileServer)
-mux.HandleFunc("/", handleIndex)
+	gmAPIKey = os.Getenv("GM_API_KEY")
+	datalasticAPIKey = os.Getenv("DATALASTIC_API_KEY")
+	staticFS := http.FS(static)
+	staticFileServer := http.FileServer(staticFS)
+	var err error
+	indexTemplate, err = template.ParseFS(templates, "templates/index.html")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	mux := http.NewServeMux()
+	mux.Handle("/static/", staticFileServer)
+	mux.HandleFunc("/", handleIndex)
 
-mux.HandleFunc("/api/get-vessels", handleGetVessels)
+	mux.HandleFunc("/api/get-vessels", handleGetVessels)
 
-http.ListenAndServe(":8080", mux)
+	http.ListenAndServe(":8080", mux)
 }
 
 func handleGetVessels(w http.ResponseWriter, r *http.Request) {
 	lat := r.URL.Query().Get("lat")
 	_, err := strconv.ParseFloat(lat, 64)
 	if err != nil {
-	 w.Write([]byte("lat has wrong format"))
-	 w.WriteHeader(http.StatusBadRequest)
-	 return
+		w.Write([]byte("lat has wrong format"))
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
 	lon := r.URL.Query().Get("lon")
 	_, err = strconv.ParseFloat(lon, 64)
 	if err != nil {
-	 w.Write([]byte("lon has wrong format"))
-	 w.WriteHeader(http.StatusBadRequest)
-	 return
+		w.Write([]byte("lon has wrong format"))
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
 	values := url.Values{}
-	values.Add("lat", lat)
-	values.Add("lon", lon)
-	values.Add("radius", "50")
+	// values.Add("lat", lat)
+	// values.Add("lon", lon)
+	values.Add("radius", "1")
+	values.Add("port_unlocode", "USGLS")
 	values.Add("api-key", datalasticAPIKey)
-	resp, err := http.Get(datalasticBaseURL + values.Encode() + datalasticAdditonalParams)
+	resp, err := http.Get(datalasticBaseURL + values.Encode())
 	if err != nil {
-	 w.Write([]byte("unable to get vessels"))
-	 w.WriteHeader(http.StatusInternalServerError)
-	 return
+		w.Write([]byte("unable to get vessels"))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 	if resp.StatusCode != http.StatusOK {
-	 w.Write([]byte("unable to get vessels"))
-	 w.WriteHeader(http.StatusInternalServerError)
-	 return
+		w.Write([]byte("unable to get vessels"))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 	io.Copy(w, resp.Body)
 	defer resp.Body.Close()
 }
 
 func handleIndex(w http.ResponseWriter, r *http.Request) {
-_ = indexTemplate.Execute(w, gmAPIKey)
-} 
+	_ = indexTemplate.Execute(w, gmAPIKey)
+}
--- static/index.js
+++ static/index.js
@@ -2,88 +2,100 @@ let map;
 let markers = [];
 let infoWindow;
 function initMap() {
- map = new google.maps.Map(document.getElementById("map"), {
- zoom: 2,
- zoomControl: true,
- scaleControl: true,
- center: { lat: 0, lng: 0 },
- });
- infoWindow = new google.maps.InfoWindow();
- map.addListener("click", (e) => {
- deleteMarkers()
- addCenterMarker(e.latLng)
- getVessels(e.latLng)
- });
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 12,
+        zoomControl: true,
+        scaleControl: true,
+        center: { lat: 29.2946, lng: -94.6746 },
+    });
+    infoWindow = new google.maps.InfoWindow();
+    // map.addListener("click", (e) => {
+    //     deleteMarkers()
+    //     addCenterMarker(e.latLng)
+    //     getVessels(e.latLng)
+    // });
+    getVessels(map.center)
 }
 function getVessels(latLng) {
- const config = {
- method: 'get',
- baseUrl: window.location.origin,
- url: 'api/get-vessels',
- params: {
- lat: latLng.toJSON().lat,
- lon: latLng.toJSON().lng,
- },
- timeout: 10000,
- }
- axios(config).then(function (response) {
- for (let i = 0; i < response.data.data.vessels.length; i++) {
- let vessel = response.data.data.vessels[i];
- addVesselMarker(vessel);
- }
- });
+    const config = {
+    method: 'get',
+    baseUrl: window.location.origin,
+    url: 'api/get-vessels',
+    params: {
+    lat: latLng.toJSON().lat,
+    lon: latLng.toJSON().lng,
+    },
+    timeout: 10000,
+    }
+    axios(config).then(function (response) {
+        for (let i = 0; i < response.data.data.vessels.length; i++) {
+            let vessel = response.data.data.vessels[i];
+            addVesselMarker(vessel);
+        }
+    });
 }
 function addVesselMarker(vessel) {
- const marker = new google.maps.Marker({
- position: {
- lat: vessel.lat,
- lng: vessel.lon,
- },
- map,
- title: vessel.name,
- });
- marker.addListener("click", () => {
- infoWindow.close();
- infoWindow.setContent(
- "<b>Name: </b> " +
- vessel.name +
- "<br><b>MMSI: </b> " +
- vessel.mmsi +
- "<br><b>Lat: </b> " +
- vessel.lat +
- "<br><b>Lng: </b> " +
- vessel.lon +
- "<br><b>Speed: </b> " +
- vessel.speed +
- "<br><b>Course: </b> " +
- vessel.course
- );
- infoWindow.open({
- anchor: marker,
- map,
- });
- });
- markers.push(marker);
+    const marker = new google.maps.Marker({
+        position: {
+            lat: vessel.lat,
+            lng: vessel.lon,
+        },
+        map,
+        title: vessel.name,
+        icon: {
+            path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW, // Icon URL
+            scale: 3,
+            fillColor: "red",  // Change the fill color to red
+            fillOpacity: 1,  // Adjust the fill opacity as needed
+            strokeWeight: 1,
+            scaledSize: new google.maps.Size(32, 32),  // Adjust the size of the icon
+            rotation: vessel.course, // Rotate the icon
+        },
+    });
+    marker.addListener("click", () => {
+        infoWindow.close();
+        infoWindow.setContent(
+        "<b>Name: </b> " +
+        vessel.name +
+        "<br><b>MMSI: </b> " +
+        vessel.mmsi +
+        "<br><b>Lat: </b> " +
+        vessel.lat +
+        "<br><b>Lng: </b> " +
+        vessel.lon +
+        "<br><b>Speed: </b> " +
+        vessel.speed +
+        "<br><b>Course: </b> " +
+        vessel.course +
+        "<br><b>Destination: </b> " +
+        vessel.destination
+        );
+        infoWindow.open({
+            anchor: marker,
+            map,
+        });
+    });
+    markers.push(marker);
 }
 function addCenterMarker(latLng) {
- const marker = new google.maps.Marker({
- position: latLng,
- map,
- title: "Center",
- });
- marker.addListener("click", () => {
- infoWindow.close();
- infoWindow.setContent(
- JSON.stringify(latLng.toJSON(), null, 2)
- );
- infoWindow.open({
- anchor: marker,
- map,
- });
- });
- markers.push(marker);
+    const marker = new google.maps.Marker({
+        position: latLng,
+        map,
+        title: "Center",
+    });
+    marker.addListener("click", () => {
+        infoWindow.close();
+        infoWindow.setContent(
+        JSON.stringify(latLng.toJSON(), null, 2)
+        );
+        infoWindow.open({
+            anchor: marker,
+            map,
+        });
+    });
+    markers.push(marker);
 }
 function deleteMarkers() {
- //We skipped this function to make this code block shorter
+    //We skipped this function to make this code block shorter
 }
 window.initMap = initMap;
